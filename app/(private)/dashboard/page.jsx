327e672ac3cd014d5b501bd3aaa6d60ad2cdffbd
Padam <subbapdm@gmail.com>
2024-10-02 23:32:43 +0200

    blog section created

--- app/(private)/dashboard/page.jsx
+++ app/(private)/dashboard/page.jsx
@@ -4,6 +4,7 @@ import CardDefault from '@/components/dashboard/cards/CardDefault';
 import { ChartSpline, Eye, ShoppingBag, Users } from 'lucide-react';
 
 import dynamic from 'next/dynamic';
+import Image from 'next/image';
 
 const Bar = dynamic(() => import('@/components/dashboard/charts/Bar'), {
     ssr: false
@@ -29,10 +30,10 @@ const Dashboard = () => {
 
         <div className='col-span-7 rounded-xl'>
           <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-5'>
-            <CardDefault title="Total customer" Icon={Users} number="509600" percent="2.5" background="#0891B2" />
-            <CardDefault title="Total revenue" Icon={ChartSpline} number="96500" percent="2.5" chart={ <Line width={100} height={100} /> }/>
-            <CardDefault title="Total visitors" Icon={Eye} number="596200" percent="2.5" chart={ <Line width={100} height={100} /> }/>
-            <CardDefault title="Total orders" Icon={ShoppingBag} number="96300" percent="2.5" chart={ <Line width={100} height={100} /> }/>
+            <CardDefault title="Total customer" icon={Users} number="509600" percent="2.5" variant="dark"/>
+            <CardDefault title="Total revenue" icon={ChartSpline} number="96500" percent="2.5"/>
+            <CardDefault title="Total visitors" icon={Eye} number="596200" percent="2.5"/>
+            <CardDefault title="Total orders" icon={ShoppingBag} number="96300" percent="2.5"/>
           </div>
 
           <div className='bg-white col-span-5 rounded-xl mt-5'>
@@ -71,9 +72,59 @@ const Dashboard = () => {
         <div className='col-span-3 rounded-xl'>
 
           <div className='bg-white col-span-3 rounded-xl'>
+          
+            <div className='p-4'>
+              <h2 className='text-xl font-bold mb-2'>Latest Blogs</h2>
+            </div>
+
+            <div className='flex justify-beteween items-center gap-2 mb-3'>
+              <div className='w-12 h-12 relative overflow-hidden rounded-md'>
+                <Image
+                    fill
+                    alt="Listing"
+                    src="/images/thumb-1.jpg"
+                    className="object-cover h-full w-full transition"
+                />
+              </div>
+              <div>
+                <h2 className='text-sm text-gray-700 font-[600] mb-2'>Beautiful Day with Friends</h2>
+                <p className='text-xs font-medium text-gray-400'>24 Oct, 2024</p>
+              </div>
+            </div>
+
+            <div className='flex justify-beteween items-center gap-2 mb-3'>
+              <div className='w-12 h-12 relative overflow-hidden rounded-md'>
+                <Image
+                    fill
+                    alt="Listing"
+                    src="/images/thumb-3.jpg"
+                    className="object-cover h-full w-full transition"
+                />
+              </div>
+              <div>
+                <h2 className='text-sm text-gray-700 font-[600] mb-2'>Mountain Trekking Blogs</h2>
+                <p className='text-xs font-medium text-gray-400'>24 Oct, 2024</p>
+              </div>
+            </div>
+
+            <div className='flex justify-beteween items-center gap-2 mb-3'>
+              <div className='w-12 h-12 relative overflow-hidden rounded-md'>
+                <Image
+                    fill
+                    alt="Listing"
+                    src="/images/thumb-2.jpg"
+                    className="object-cover h-full w-full transition"
+                />
+              </div>
+              <div>
+                <h2 className='text-sm text-gray-700 font-[600] mb-2'>Session vs token based authentication</h2>
+                <p className='text-xs font-medium text-gray-400'>24 Oct, 2024</p>
+              </div>
+            </div>
             
           </div>
           <div className='bg-white col-span-3 rounded-xl'>
+
             <div className='p-4'>
               <h2 className='text-xl font-bold mb-2'>Pie Chart</h2>
               <p className='text-sm text-gray-300'>Showing total visitors for the last 6 months</p>
--- components/dashboard/cards/CardDefault.jsx
+++ components/dashboard/cards/CardDefault.jsx
@@ -1,27 +1,27 @@
 import { TrendingUp } from 'lucide-react';
 
-const CardDefault = ({ title, Icon, number, percent, background, chart }) => {
+const CardDefault = ({ title, icon: Icon, number, percent, background, variant }) => {
 
     return (
-        <div className='bg-white rounded-xl flex p-4' style={{ backgroundColor: background }}>
+        <div className={`rounded-xl flex p-4 ${variant === 'dark' ? 'bg-cyan-700' : 'bg-white '}`}>
             <div className='flex-2'>
-                <div className='flex items-center gap-2 text-gray-400 mb-3'>
-                    <div className={`${background && 'bg-cyan-700 text-white'} rounded-full bg-gray-100 text-gray-400 p-2`}>
-                        <Icon size={15}/>
+                <div className='flex items-center gap-2 text-gray-400 font-medium mb-3'>
+                    <div className={`${variant === 'dark' ? 'bg-cyan-600 text-white' : 'bg-gray-100 text-cyan-500'} rounded-full p-2`}>
+                        <Icon size={20}/>
                     </div>
-                    <span className={`${background && 'text-white'} text-xs`}>{title}</span>
+                    <span className={`${variant === 'dark' ? 'text-white' : ''} text-sm`}>{title}</span>
                 </div>
                 <div className='flex flex-col gap-3'>
-                    <h3 className={`${background && 'text-white'} text-[25px] font-extrabold text-gray-700`}>${number}</h3>
-                    <div className={`${background && 'text-teal-300'} flex items-center gap-2 text-xs text-emerald-400`}>
+                    <h3 className={`${variant === 'dark' ? 'text-white' : 'text-gray-800'} text-[25px] font-extrabold`}>${number}</h3>
+                    <div className={`${variant === 'dark' && 'text-teal-300'} flex items-center gap-2 text-xs text-emerald-400`}>
                         <TrendingUp size={15}/>
                         <span className='font-bold'>{percent}%</span>
-                        <span className={`${background && 'text-white'} text-xs text-gray-300`}>+2.5 from last month</span>
+                        <span className={`${variant === 'dark' && 'text-white'} text-xs text-gray-300`}>+2.5 from last month</span>
                     </div>
                 </div>
             </div>
             <div className='flex-1'>
-                {chart}
+                
             </div>
         </div>
     )
