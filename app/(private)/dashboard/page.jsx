2c2025d1d43d7a87dc5ab885a049d424d792429d
Padam <subbapdm@gmail.com>
2025-08-15 00:33:36 +0200

    css changes

--- app/(private)/dashboard/page.jsx
+++ app/(private)/dashboard/page.jsx
@@ -26,7 +26,7 @@ const Dashboard = () => {
   return (
     <div className='w-full flex flex-col gap-5'>
 
-      <div className='grid grid-cols-10 gap-5'>
+      <div className='grid grid-cols-1 md:grid-cols-10 gap-5'>
 
         <div className='col-span-7 rounded-xl'>
           <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-5'>
@@ -44,7 +44,7 @@ const Dashboard = () => {
             <Bar width={100} height={400} />
           </div>
 
-          <div className='grid grid-cols-2 gap-5 mt-5'>
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-5 mt-5'>
 
             <div className='bg-white rounded-xl'>
               <div className='p-4'>
@@ -69,64 +69,66 @@ const Dashboard = () => {
 
         </div>
 
-        <div className='col-span-3 rounded-xl'>
+        <div className='col-span-7 md:col-span-3 rounded-xl'>
 
-          <div className='bg-white col-span-3 rounded-xl'>
+          <div className='bg-white md:col-span-3 rounded-xl mb-5'>
           
             <div className='p-4'>
-              <h2 className='text-xl font-bold mb-2'>Latest Blogs</h2>
+              
+              <div>
+                <h2 className='text-xl font-bold mb-2'>Latest Blogs</h2>
+              </div>
 
               <div className='flex justify-beteween items-center gap-2 mb-3'>
-              <div className='w-12 h-12 relative overflow-hidden rounded-md'>
-                <Image
-                    fill
-                    alt="Listing"
-                    src="/images/thumb-1.jpg"
-                    className="object-cover h-full w-full transition"
-                />
+                <div className='w-12 h-12 relative overflow-hidden rounded-md'>
+                  <Image
+                      fill
+                      alt="Listing"
+                      src="/images/thumb-1.jpg"
+                      className="object-cover h-full w-full transition"
+                  />
+                </div>
+                <div>
+                  <h2 className='text-sm text-gray-700 font-[600] mb-2'>Beautiful Day with Friends</h2>
+                  <p className='text-xs font-medium text-gray-400'>24 Oct, 2024</p>
+                </div>
               </div>
-              <div>
-                <h2 className='text-sm text-gray-700 font-[600] mb-2'>Beautiful Day with Friends</h2>
-                <p className='text-xs font-medium text-gray-400'>24 Oct, 2024</p>
+
+              <div className='flex justify-beteween items-center gap-2 mb-3'>
+                <div className='w-12 h-12 relative overflow-hidden rounded-md'>
+                  <Image
+                      fill
+                      alt="Listing"
+                      src="/images/thumb-3.jpg"
+                      className="object-cover h-full w-full transition"
+                  />
+                </div>
+                <div>
+                  <h2 className='text-sm text-gray-700 font-[600] mb-2'>Mountain Trekking Blogs</h2>
+                  <p className='text-xs font-medium text-gray-400'>24 Oct, 2024</p>
+                </div>
               </div>
+
+              <div className='flex justify-beteween items-center gap-2 mb-3'>
+                <div className='w-12 h-12 relative overflow-hidden rounded-md'>
+                  <Image
+                      fill
+                      alt="Listing"
+                      src="/images/thumb-2.jpg"
+                      className="object-cover h-full w-full transition"
+                  />
+                </div>
+                <div>
+                  <h2 className='text-sm text-gray-700 font-[600] mb-2'>Session vs token based authentication</h2>
+                  <p className='text-xs font-medium text-gray-400'>24 Oct, 2024</p>
+                </div>
+              </div>
+
             </div>
 
-            <div className='flex justify-beteween items-center gap-2 mb-3'>
-              <div className='w-12 h-12 relative overflow-hidden rounded-md'>
-                <Image
-                    fill
-                    alt="Listing"
-                    src="/images/thumb-3.jpg"
-                    className="object-cover h-full w-full transition"
-                />
-              </div>
-              <div>
-                <h2 className='text-sm text-gray-700 font-[600] mb-2'>Mountain Trekking Blogs</h2>
-                <p className='text-xs font-medium text-gray-400'>24 Oct, 2024</p>
-              </div>
-            </div>
-
-            <div className='flex justify-beteween items-center gap-2 mb-3'>
-              <div className='w-12 h-12 relative overflow-hidden rounded-md'>
-                <Image
-                    fill
-                    alt="Listing"
-                    src="/images/thumb-2.jpg"
-                    className="object-cover h-full w-full transition"
-                />
-              </div>
-              <div>
-                <h2 className='text-sm text-gray-700 font-[600] mb-2'>Session vs token based authentication</h2>
-                <p className='text-xs font-medium text-gray-400'>24 Oct, 2024</p>
-              </div>
-            </div>
-            </div>
-
-            
-            
           </div>
 
-          <div className='bg-white col-span-3 rounded-xl'>
+          <div className='bg-white md:col-span-3 rounded-xl'>
 
             <div className='p-4'>
               <h2 className='text-xl font-bold mb-2'>Pie Chart</h2>
